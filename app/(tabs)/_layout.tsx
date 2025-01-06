2e3dcfe0c80a31185135c6f644e34f9950eb0891
Tevye Cowan <t.b.cowan@gmail.com>
2025-01-06 08:10:08 -0800

    friday - WIP tab screen investigation - infobar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -21,7 +21,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Hero',
+          title: 'Heroh',
           tabBarIcon: ({ color, focused }) => (
             <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
           ),
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,27 @@
 import { View, StyleSheet } from 'react-native';
-
+import * as React from 'react';
 import Button from '@/components/Button';
 import MaterialButton from '@/components/MaterialButton';
-
+import InfoBar from '@/components/InfoBar'
 const PlaceholderImage = require('@/assets/images/background-image.png');
+const stuff = (<div className="stuff">some stuff</div>);
+
+
 
 export default function Index() {
   return (
     <View style={styles.container}>
+      <InfoBar/>
       <View style={styles.footerContainer}>
          <MaterialButton/>
+         
+         <div className="blah">
+          some text here
+         </div>
+         <div>
+          some more text here
+         </div>
+
       </View>
     </View>
   );
